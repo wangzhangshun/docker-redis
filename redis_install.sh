91f5c405dd6284f7b27b8f9d97e7331c1bb89670
ken.wang <ken.wang@winway666.com>
2023-06-05 18:57:58 +0800

    设置配置文件 2

--- redis_install.sh
+++ redis_install.sh
@@ -8,3 +8,5 @@ make
 make PREFIX=/usr/local/redis install
 mkdir -p /usr/local/redis/conf/
 cp /home/redis.conf /usr/local/redis/conf/
+sed -i '69s/127.0.0.1/0.0.0.0/' /usr/local/redis/conf/redis.conf
+sed -i '88s/protected-mode yes/protected-mode no/' /usr/local/redis/conf/redis.conf
